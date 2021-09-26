650706b83ff1b974c33e712254ecefe2e20ca427
Brennan Fife <brenfife@gmail.com>
2021-09-26 13:11:59 -0600

    Update

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import Head from 'next/head';
+
+// import Layout from '../components/Layout';
+// import LayoutProvider from '../context';
+
+export default function App({ Component, pageProps }: any) {
+  return (
+    <>
+      <Head>
+        <title>NFT Activity</title>
+        <meta name="description" content="NFT Activity" />
+        {/* <link rel="shortcut icon" href="/favicon.png" /> */}
+      </Head>
+      {/* <LayoutProvider> */}
+      <ChakraProvider>
+        {/* <Layout> */}
+        <Component {...pageProps} />
+        {/* </Layout> */}
+      </ChakraProvider>
+      {/* </LayoutProvider> */}
+    </>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return <h1>hre</h1>;
+};
+
+export default Home;
